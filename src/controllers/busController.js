a5e4bcfb29f215d3b5ce3affa94dc1c3ebbb5254
CuriousGeorg1 <georg.m.wahlroos@gmail.com>
2025-03-20 14:29:59 +0200

    [feature] endpoint for getting info for a specific bus stop

--- src/controllers/busController.js
+++ src/controllers/busController.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import { getBusStops } from "../services/busService.js";
+import { getBusStops, getBusStop } from "../services/busService.js";
 
 const busController = Router();
 
+// Controller for handling rewuests for bus stations
 busController.get("/busStations", async (req, res) => {
   const busStations = await getBusStops();
-  res.json(busStations);
+  if (busStations) {
+    res.status(200).json(busStations);
+  } else {
+    res.status(404).json({ error: "Bus stations were not found" });
+  }
+});
+
+// Controller for handling requests for a single bus station
+busController.get("/busStations/:id", async (req, res) => {
+  const { id } = req.params;
+  const busStation = await getBusStop(id);
+  res.json(busStation);
 });
 
 export default busController;
--- src/services/busService.js
+++ src/services/busService.js
@@ -5,7 +5,30 @@ dotenv.config();
 
 const busApi = process.env.API_URI;
 
+/*
+  This function will fetch the bus stops from the API
+  and return the data.
+
+  format:
+  {
+  "1": {
+    "stop_name": "Turun satama (Silja)"
+  }
+}
+*/
 export async function getBusStops() {
-  const response = await axios.get(busApi);
+  const response = await axios.get(`${busApi}/sm/pretty`);
   return response.data;
+}
+
+export async function getBusStop(stopId) {
+  try {
+    var url = `${busApi}/sm/${stopId}/pretty`;
+    console.log(`${busApi}/sm/${stopId}/pretty`);
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return JSON.stringify({ error: "Bus stop not found", status: 404 });
+  }
 }
