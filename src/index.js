7374ab2e897c997170c9f0bd37685f530193b629
CuriousGeorg1 <georg.m.wahlroos@gmail.com>
2025-03-20 19:53:06 +0200

    [patch] cross-origin requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import busController from "./controllers/busController.js";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use("/api", busController);
